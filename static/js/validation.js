8ddbf34095546aa9ee3a6356aff1be952e0d7325
PhilEllis <philippamacnab@hotmail.com>
2023-11-09 23:59:06 +0000

    Fix!: JS code updated - carousel still not working

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,25 +1,43 @@
-$(document).ready(function() {
-    // Disable the "Book Now" button initially
-    $("#bookNowButton").prop("disabled", true);
-
-    // Listen for changes in the location dropdown
-    $("#locationDropdown").change(function() {
+document.addEventListener('DOMContentLoaded', function() {
+    //  "Book Now" button
+    var bookNowButton = document.getElementById('bookNowButton');
+    var locationDropdown = document.getElementById('locationDropdown');
+    
+    // Disable the Book Now button initially
+    bookNowButton.disabled = true;
+    
+    // Listen for changes in location 
+    locationDropdown.addEventListener('change', function() {
         // Check if a location is selected
-        if ($(this).val() !== "") {
-            // Enable the "Book Now" button
-            $("#bookNowButton").prop("disabled", false);
-        } else {
-            // Disable the "Book Now" button
-            $("#bookNowButton").prop("disabled", true);
-        }
+        bookNowButton.disabled = this.value === "";
     });
-
-    // Listen for a click on the "Book Now" button
-    $("#bookNowButton").click(function() {
+    
+    // Listen for a click on the Book Now button 
+    bookNowButton.addEventListener('click', function() {
         // Check if a location is selected
-        if ($("#locationDropdown").val() === "") {
+        if (locationDropdown.value === "") {
             // Display a warning message using an alert
             alert("Select a location before booking.");
         }
     });
+
+    // Bootstrap 5 Carousel Initialization
+    var heroCarouselElement = document.querySelector('#heroCarousel');
+    if (heroCarouselElement) {
+        var heroCarousel = new bootstrap.Carousel(heroCarouselElement, {
+            interval: 5000,
+            ride: 'carousel'
+        });
+    }
+
+    // AOS Initialization
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    });
+
+    // PureCounter Initialization
+    new PureCounter();
 });
