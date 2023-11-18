73d6163a46ead0f35963da779b45e8085e53423b
PhilEllis <philippamacnab@hotmail.com>
2023-11-18 20:25:21 +0000

    Fix!: Contexts fix NoneType error

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,40 +4,33 @@ from django.shortcuts import get_object_or_404
 from products.models import Course
 
 def bag_contents(request):
-
     bag_items = []
-    total = 0
+    total = Decimal('0.00')
     product_count = 0
     bag = request.session.get('bag', {})
 
     for course_id, quantity in bag.items():
-        product = get_object_or_404(Course, pk=course_id)
-        total = Decimal('0.00')
+        course = get_object_or_404(Course, pk=course_id)
+        total += course.price * quantity  # Calculate total for each item
+        product_count += quantity  # Update product count
 
-        if bag:
-            # Assuming the bag contains only one item with its course_id and quantity
-            course_id, quantity = list(bag.items())[0]
-
-            course = get_object_or_404(Course, pk=course_id)
-            total = course.price * quantity  # Calculate total
-
-            bag_items.append({
-                'course_id': course_id,
-                'name': course.name,
-                'level': course.get_level_display(),
-                'start_date': course.start_date,
-                'location_name': course.location.name,
-                'quantity': quantity,
-                'product': product,
-            })
+        bag_items.append({
+            'course_id': course_id,
+            'name': course.name,
+            'level': course.get_level_display(),
+            'start_date': course.start_date,
+            'location_name': course.location.name,
+            'quantity': quantity,
+            'product': course,  # Use course directly
+        })
     
-        grand_total = total
+    grand_total = total
 
-        context = {
-            'bag_items': bag_items,
-            'total': total,
-            'product_count': product_count,
-            'grand_total': grand_total,
-        }
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
 
-        return context
+    return context
