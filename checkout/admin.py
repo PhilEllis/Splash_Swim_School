467370113f96afc21bc83b31a961cfdba542c0c1
PhilEllis <philippamacnab@hotmail.com>
2023-11-28 23:05:25 +0000

    Feat!: Order history connect to profile

--- checkout/admin.py
+++ checkout/admin.py
@@ -18,8 +18,8 @@ class OrderAdmin(admin.ModelAdmin):
         return ", ".join([line_item.course.name for line_item in obj.lineitems.all()])
     get_course_names.short_description = 'Courses'
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 
-              'country', 'postcode', 'town_or_city', 
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 'email', 
+              'phone_number', 'country', 'postcode', 'town_or_city', 
               'street_address1', 'street_address2', 'county', 
               'order_total', 'grand_total', 'get_course_names', 
               'original_bag', 'stripe_pid')
