d14bcbda8e0de71b7ebb02a2ad21bf3176987393
PhilEllis <philippamacnab@hotmail.com>
2023-12-04 23:20:34 +0000

    Fix!: Separate form processing & btn name attribte

--- profiles/views.py
+++ profiles/views.py
@@ -8,45 +8,49 @@ from checkout.models import Order
 
 
 def profile(request):
-    """ Display the user's profile. """
     user_profile = get_object_or_404(UserProfile, user=request.user)
+    user_form = UserProfileForm(instance=user_profile)
+    orders = user_profile.orders.all()
+
     guardian_profile, created = GuardianProfile.objects.get_or_create(user=request.user)
+    guardian_form = GuardianProfileForm(instance=guardian_profile)
+    child_form = ChildProfileForm()
 
     if request.method == 'POST':
-        user_form = UserProfileForm(request.POST, instance=user_profile)
-        guardian_form = GuardianProfileForm(request.POST, instance=guardian_profile)
-        child_form = ChildProfileForm(request.POST)
+        if 'update_user' in request.POST:
+            user_form = UserProfileForm(request.POST, instance=user_profile)
+            if user_form.is_valid():
+                user_form.save()
+                messages.success(request, 'Profile updated successfully')
 
-        if user_form.is_valid() and guardian_form.is_valid():
-            user_form.save()
-            guardian_form.save()
-            messages.success(request, 'Profile updated successfully')
+        if 'update_guardian' in request.POST:
+            guardian_form = GuardianProfileForm(request.POST, instance=guardian_profile)
+            if guardian_form.is_valid():
+                guardian_form.save()
+                messages.success(request, 'Guardian profile updated successfully')
+
+        if 'add_child' in request.POST:
+            child_form = ChildProfileForm(request.POST)
             if child_form.is_valid():
                 child = child_form.save(commit=False)
                 child.guardian = guardian_profile
                 child.save()
-                messages.success(request, f'Child {child.name} added successfully')
-                return redirect('profile')
-        else:
-            messages.error(request, 'There was an error with the form submission.')
-    else:
-        user_form = UserProfileForm(instance=user_profile)
-        guardian_form = GuardianProfileForm(instance=guardian_profile)
-        child_form = ChildProfileForm()
+                messages.success(request, f'Child profile for {child.name} added successfully')
+                return redirect('profile')  # Redirect to clear the form
 
-    orders = user_profile.orders.all()
+    children = ChildProfile.objects.filter(guardian=guardian_profile)
 
-    template = 'profiles/profile.html'
     context = {
         'user_form': user_form,
         'guardian_form': guardian_form,
         'child_form': child_form,
+        'children': children,
         'orders': orders,
-        'on_profile_page': True,
-        'children': ChildProfile.objects.filter(guardian=guardian_profile)
+        'on_profile_page': True
     }
 
-    return render(request, template, context)
+    return render(request, 'profiles/profile.html', context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
