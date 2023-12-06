e654fee98550ef17061b4bae7742814a327ec48c
PhilEllis <philippamacnab@hotmail.com>
2023-12-06 15:41:58 +0000

    Fix!: updated allowed host

--- splash/settings.py
+++ splash/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['splash-swim-school.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['splash-swim-school-b17312c07dec.herokuapp.com', 'localhost']
 
 
 # Application definition
