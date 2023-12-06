14e8327555611ea385e752e614c17fda13f0b95b
PhilEllis <philippamacnab@hotmail.com>
2023-12-06 11:44:55 +0000

    Feat!: Admin view for child with gaurdian details

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ChildProfile, GuardianProfile
+
+
+class ChildProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_of_birth', 'guardian_info', 'confidence_in_water', 'medical_conditions', 'medication')
+
+    def guardian_info(self, obj):
+        guardian = obj.guardian
+        return f"{guardian.contact_name} ({guardian.relationship_to_child}), Emergency Contact: {guardian.emergency_contact_number}"
+    guardian_info.short_description = 'Guardian Details'
 
 # Register your models here.
+admin.site.register(ChildProfile, ChildProfileAdmin)
