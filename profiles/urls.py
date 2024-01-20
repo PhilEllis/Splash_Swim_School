3a1e8667926b0e820ea1206dfcc74771ca24a031
PhilEllis <philippamacnab@hotmail.com>
2024-01-20 22:38:29 +0000

    Fix!: Urls.py pep8 code correction

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,8 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path(
+        'order_history/<order_number>',
+        views.order_history,
+        name='order_history'
+    ),
     path('delete_guardian/', views.delete_guardian, name='delete_guardian'),
-    path('update-child/<int:child_id>/', views.update_child_profile, name='update_child_profile'),
-    path('delete-child/<int:child_id>/', views.delete_child_profile, name='delete_child_profile'),
+    path(
+        'update-child/<int:child_id>/',
+        views.update_child_profile,
+        name='update_child_profile'
+    ),
+    path(
+        'delete-child/<int:child_id>/',
+        views.delete_child_profile,
+        name='delete_child_profile'
+    ),
 ]
