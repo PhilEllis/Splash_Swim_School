e119ac77cf0d48bdd3ccd0bdec7ef252698e785b
PhilEllis <philippamacnab@hotmail.com>
2024-01-24 22:27:49 +0000

    Fix!: Locations app docstrings updated

--- locations/apps.py
+++ locations/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class LocationsConfig(AppConfig):
+    """
+    AppConfig for the Locations app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'locations'
--- locations/models.py
+++ locations/models.py
@@ -3,8 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
+    """
+    Model representing a location and it's max capacity.
+    """
+
     name = models.CharField(max_length=100)
     capacity = models.PositiveIntegerField()
 
     def __str__(self):
+        """
+        Return the location name as its string representation.
+        """
         return self.name
