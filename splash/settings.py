1c8b5158a001a31ab383622edf4688d6001e030f
PhilEllis <philippamacnab@hotmail.com>
2024-01-25 23:39:19 +0000

    Feat!: 404 & 500 error pages styled

--- splash/settings.py
+++ splash/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['splash-swim-school-b17312c07dec.herokuapp.com', 'localhost',
                  '8000-philellis-splashswimsch-ujl9qe9u73r.ws-eu107.gitpod.io']
