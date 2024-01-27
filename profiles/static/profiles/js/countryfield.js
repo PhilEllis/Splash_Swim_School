8cf9b8bd50d4d5ae037a1630f463aee8faa1ee77
PhilEllis <philippamacnab@hotmail.com>
2024-01-27 23:10:47 +0000

    Fix!: Docstring added to js countryfield file

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,3 +1,15 @@
+/**
+ * Handles the styling of the country selection dropdown in the profile form.
+ * 
+ * Checks if a country has been selected in the country dropdown 
+ * If no country is selected, it sets the dropdown's 
+ * text color to a light grey to act as a placeholder style.
+ * 
+ * It adds a change event listener to the country dropdown. When a change 
+ * in selection occurs, it checks if a country is selected. If not, it retains the grey 
+ * text color. Otherwise, it changes the text color to a darker shade.
+ * 
+ */
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
     $('#id_default_country').css('color', '#aab7c4');
